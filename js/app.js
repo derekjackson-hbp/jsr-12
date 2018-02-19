4b33c30e3e38b501f46e55d69bae193a94df30a7
derekjackson-hbp <derek.jackson@hbsp.harvard.edu>
2018-02-19 18:50:47 -0500

    add handlebars and loader image

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 /*
   Please add all Javascript code to this file.
 */
-var nytList
+var nytList;
+var articles = {};
+var source   = $('#template').html();
+var template = Handlebars.compile(source);
 
 var time = setTimeout(function(){
   $('.loader').toggle();
@@ -17,17 +20,48 @@ var apis = {
     url += '?' + $.param({
       'api-key': "67440d21b3a04fe49f4211828a60f982"
     });
-    return $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function(result) {
-      nytList = result.response.docs;
-
-      console.log(result);
-      {debugger}
-    }).fail(function(err) {
-      throw err;
-    });
+    $.ajax({
+          url: url,
+          method: 'GET',
+          success: function(result){
+            articles = result.response.docs
+            articles.forEach(function(a){
+              var link = a.web_url;
+              var hed = a.headline.main;
+              var abstract = a.snippet;
+              var source = a.byline.original;
+              var words = a.word_count;
+              var type = a.type_of_material;
+              var image_source = a.multimedia[0];
+              if (image_source){
+                var image = 'https://static01.nyt.com/' + a.multimedia[0].url;
+              }
+              else {
+                var image = '../images/Logo-NYT.jpg';
+              };
+              var innerTemp = {
+                link : a.web_url,
+                hed : a.headline.main,
+                abstract : a.snippet,
+                pic : image
+              };
+              var html = template(innerTemp)
+              $('#main').append(html)
+              console.log(link + '\b ' + hed + '\b ' + abstract + '\b ' + source + '\b ' + words + '\b ' + type + '\b ' + image);
+            });},
+            complete: function(){
+              $('.loader').toggle();
+              {debugger};
+              $('#main').toggle();}
+        })/*.done(function(result) {
+          console.log(result);
+          articles = {
+            hed : result.response.docs
+          }
+          {debugger}
+        }).fail(function(err) {
+          throw err;
+        });*/
   },
 };
 
@@ -43,7 +77,9 @@ var showTitles = function(arr){
 
 // add event handler for the news sources
 $('nav').on('click','ul ul li',function(){
+  $('#main').toggle();
+
+  $('.loader').toggle();
   var source = $(this).data('api');
-  {debugger};
-  return apis[source]();
+  var read = apis[source]();
 });
